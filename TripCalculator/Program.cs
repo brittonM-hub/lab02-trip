a008a6fe0ece97c5e190f439df7dda8cab03755f
Matthew Britton <britton.matthew@icloud.com>
2026-09-25 10:14:20 -0400

    Part 3 Paycheck

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("What was the round trip in miles?");
+﻿//Part 1 Road Trip
+Console.Write("What was the round trip in miles?");
 int milesForTheTrip = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("What is the mileds per gallon of the car you are using? ");
@@ -38,4 +39,19 @@ Console.WriteLine("Pizza Cost: " + pizzaCost.ToString("C"));
 
 //Part 3 PayCheck
 Console.Write("How many hours worked this week?: ");
-int hoursWorked = Convert.ToInt32(Console.ReadLine());
+int hoursWorked = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("What is yout hourly rate? ");
+decimal hourlyRate = Convert.ToDecimal(Console.ReadLine());
+
+const decimal taxRate = 0.18m;
+
+decimal grossPay = hoursWorked * hourlyRate;
+
+decimal taxWitheld = grossPay * taxRate;
+
+decimal takeHomePay = grossPay - taxWitheld;
+
+Console.WriteLine("Gross Pay: " + grossPay.ToString("C"));
+Console.WriteLine("Tax Withheld: " + taxWitheld.ToString("C"));
+Console.WriteLine("Take Home Pay: " + takeHomePay.ToString("C"));
