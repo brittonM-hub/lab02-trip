83e4fc090f7505087e87a71f839aa7e4a133039f
Matthew Britton <britton.matthew@icloud.com>
2026-09-25 10:36:09 -0400

    Part 4 The Whole Trip

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -1,8 +1,10 @@
 ﻿//Part 1 Road Trip
+using System.Security.Cryptography;
+
 Console.Write("What was the round trip in miles?");
 int milesForTheTrip = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("What is the mileds per gallon of the car you are using? ");
+Console.Write("What is the miles per gallon of the car you are using? ");
 int milesPerGallon = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("What is the gas price? ");
@@ -41,7 +43,7 @@ Console.WriteLine("Pizza Cost: " + pizzaCost.ToString("C"));
 Console.Write("How many hours worked this week?: ");
 int hoursWorked = Convert.ToInt32(Console.ReadLine()); 
 
-Console.Write("What is yout hourly rate? ");
+Console.Write("What is your hourly rate? ");
 decimal hourlyRate = Convert.ToDecimal(Console.ReadLine());
 
 const decimal taxRate = 0.18m;
@@ -55,3 +57,17 @@ decimal takeHomePay = grossPay - taxWitheld;
 Console.WriteLine("Gross Pay: " + grossPay.ToString("C"));
 Console.WriteLine("Tax Withheld: " + taxWitheld.ToString("C"));
 Console.WriteLine("Take Home Pay: " + takeHomePay.ToString("C"));
+
+//Part 4 The Whole Trip
+double tripTotal = fuelCost + pizzaCost;
+
+double costPerPerson = tripTotal / peopleGoing;
+
+double takeHomePayPerHour = (double)takeHomePay / hoursWorked;
+
+double hoursMustWork = costPerPerson / takeHomePayPerHour;
+
+Console.WriteLine("Trip Total: " + tripTotal.ToString("C"));
+Console.WriteLine("Cost per person: " + costPerPerson.ToString("C"));
+Console.WriteLine("Take home pay per hour: " + takeHomePayPerHour.ToString("C"));
+Console.WriteLine("Hours you must work to cover your share: " + hoursMustWork.ToString("F2"));
